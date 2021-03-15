aeffcacefee77854e882afbf8f9aa3f0b8aaeccf
SDMR-LAB <80406912+SDMR-LAB@users.noreply.github.com>
2021-03-15 16:07:01 +0700

    Create core_interpeter.py

---
+++ core_interpeter.py
@@ -0,0 +1,26 @@
+def run(code):
+    code = parse(code)
+    x = i = 0
+    bf = {0: 0}
+    blocks = block(code)
+    l = len(code)
+    while i < l:
+        sym = code[i]
+        if sym == '>':
+            x += 1
+            bf.setdefault(x, 0)
+        elif sym == '<':
+            x -= 1
+        elif sym == '+':
+            bf[x] += 1
+        elif sym == '-':
+            bf[x] -= 1
+        elif sym == '.':
+            print(chr(bf[x]), end='')
+        elif sym == ',':
+            bf[x] = int(input('Input: '))
+        elif sym == '[':
+            if not bf[x]: i = blocks[i]
+        elif sym == ']':
+            if bf[x]: i = blocks[i]
+        i += 1
